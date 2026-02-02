5df56e7b3dac04a882068ad468414c4ed9b97fc7
D0gg3d <45501503+MerpADer@users.noreply.github.com>
2026-02-01 23:45:20 -0500

    Added mechanic where one character of the item's name appears with each guess
    Removed display of the mystery item at the bottom
    
    Added alt text to all images displayed
    
    Version 1.0 of Isaacdle is complete!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,23 +11,23 @@ function GuessList({list})
           
           <div>
             <p>{element.name}</p>
-            <img src={element.itemImg} className="image-style"/>
+            <img src={element.itemImg} alt="Item" className="image-style"/>
           </div>
           <div>
             <p>{element.typeItem} </p>
-            <img src={"/images/resultPNGs/" + element?.comparisonResults[0] + ".png"} className="image-style"/>
+            <img src={"/images/resultPNGs/" + element?.comparisonResults[0] + ".png"} alt="type correctness" className="image-style"/>
           </div>
           <div>
             <p>{element.quality}</p>
-            <img src={"/images/resultPNGs/" + element?.comparisonResults[1] + ".png"} className="image-style"/>
+            <img src={"/images/resultPNGs/" + element?.comparisonResults[1] + ".png"} alt="quality correctness" className="image-style"/>
           </div>
           <div style={{width:"30ch", wordWrap:"break-word"}}>
             <p>{element.stats?.reduce((acc, s) => acc + s + ", ", "").slice(0, -2)}</p>
-            <img src={"/images/resultPNGs/" + element?.comparisonResults[2] + ".png"} className="image-style"/>
+            <img src={"/images/resultPNGs/" + element?.comparisonResults[2] + ".png"} alt="stats correctness" className="image-style"/>
           </div>
           <div>
             <p>{element.itemPool?.reduce((acc, s) => acc + s + ", ", "").slice(0, -2)}</p>
-            <img src={"/images/resultPNGs/" + element?.comparisonResults[3] + ".png"} className="image-style"/>
+            <img src={"/images/resultPNGs/" + element?.comparisonResults[3] + ".png"} alt="item pool correctness" className="image-style"/>
           </div>
         </div>
       ))}
@@ -67,11 +67,37 @@ function WinScreen({item, guesses})
       <div className="win-style">
         <p>Correct!</p>
         <p>Item Name: {item.name}</p>
-        <img src={item.itemImg} className="image-style"/>
+        <img src={item.itemImg} alt="mystery item" className="image-style"/>
         <p>Guesses: {guesses}</p>
       </div>
     </div>
   )
+}
+
+function updateRevealed(guesses, actualWord, currentProgressWord)
+{
+  const actualNoSpaces = actualWord.replaceAll(" ", "");
+  if(guesses < actualNoSpaces.length)
+  {
+    let charToReveal = Math.floor(Math.random() * (actualNoSpaces.length - guesses))
+
+    let index = 0;
+
+    while(index < currentProgressWord.length)
+    {
+      if(charToReveal === 0 && currentProgressWord[index] === "_")
+      {
+        return (currentProgressWord.slice(0, index) + actualWord[index] + currentProgressWord.slice(index+1));
+      }
+      if(currentProgressWord[index] === "_")
+      {
+        charToReveal--;
+      }
+      index++;
+    }
+
+  }
+  return currentProgressWord;
 }
 
 function App() {
@@ -83,9 +109,20 @@ function App() {
 
   const [numGuesses, setNumGuesses] = useState(0);
 
+  const [revealedChars, setRevealed] = useState("");
+
   useEffect( () => 
   {
-    axios.get("/api/data/byid/" + (Math.floor(Math.random() * 732) + 1).toString()).then(res => {setMysteryItem(res.data)});
+    const fetchMysteryItem = async () => 
+    {
+      const res = await axios.get("/api/data/byid/" + (Math.floor(Math.random() * 732) + 1).toString());
+
+      setMysteryItem(res.data)
+
+      setRevealed(Array.from(res.data.name).reduce((hidden, c) => c === " " ? hidden + c : hidden + "_", ""))
+    }
+    
+    fetchMysteryItem();
   }, [])
 
   const newGuess = async (guess) => {
@@ -99,6 +136,8 @@ function App() {
       const imgURL = "/images/itemPNGs/collectibles_" + idToStr + "_" + itemDetails.data.name.toLowerCase().replaceAll(" ", "") + ".png";
 
       updateList([{ ...itemDetails.data, itemImg: imgURL, comparisonResults: comparisonResults.data}, ...itemList])
+
+      setRevealed(updateRevealed(numGuesses, mysteryItem.name, revealedChars));
 
       setNumGuesses(numGuesses+1);
 
@@ -116,6 +155,8 @@ function App() {
       <p>Enter an item to guess:</p>
       <Input addItem={newGuess}/>
 
+      <p>Item name: {revealedChars}</p>
+
       <div className="label-style">
         {["Name", "Type of Item", "Quality", "Stats", "Item Pool"].map(topic => (
           <p style={{margin:0}}>{topic}</p>
@@ -126,7 +167,7 @@ function App() {
 
       <GuessList list={itemList}/>
 
-      {mysteryItem && <p>Mystery Item : {mysteryItem.name}</p>}
+      {/* {mysteryItem && <p>Mystery Item : {mysteryItem.name}</p>} */}
       
       {correct && <WinScreen item={itemList[0]} guesses={numGuesses} />}
 
